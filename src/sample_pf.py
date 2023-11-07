188346e0da3320bd3a7178e2e177eb99dcba5f19
Max <maxwangko1@gmail.com>
2023-11-06 22:20:05 -0500

    same portfolio

---
+++ src/sample_pf.py
@@ -0,0 +1,113 @@
+"""
+protfolio = {
+    Ticker : $ amount,
+    Ticker : $ amount,
+    Ticker : $ amount
+}
+
+"""
+
+
+#### High Diverse Portfolio ####
+HDp1 = {
+    "TSLA" : 1000,
+    "APPL" : 1000,
+    "COO" : 1000,
+}
+
+HDp2 = {
+    "BAC" : 1000,
+    "KO" : 1000,
+    "KHC" : 1000,
+    "AAPL" : 1000,
+    "WFC" : 1000
+}
+
+
+# very diverse
+HDp4 = {
+    "AAPL" : 1000,
+    "JNJ" : 1000,
+    "DIS" : 1000,
+    "JPM" : 1000,
+    "PG" : 1000,
+    "XOM" : 1000,
+    "HD" : 1000,
+    "VZ" : 1000,
+    "KO" : 1000,
+    "MMM" : 1000,
+}
+
+
+
+#### Mid Diverse Portfolio ####
+
+# two stock different industry
+MDp1 = {
+    "TSLA" : 5000,
+    "COO" : 5000,
+}
+
+# lots of same industry with one different
+MDp2 = {
+    "TSLA" : 1000,
+    "NIO" : 1000,
+    "GM" : 1000,
+    "COO" : 3000,
+}
+
+# different industry with different value
+MDp3 = {
+    "JNJ" : 1000,
+    "APPL" : 1000,
+    "DIS" : 2000,
+    "JPM" : 1500,
+    "PG" : 2000,
+}
+
+# same industry with different value
+MDp4 = {
+    "AMZN" : 1000,
+    "FB" : 2000,
+    "GOOGL" : 3000,
+    "NFLX" : 4000,
+}
+
+#### Low Diverse Portfolio ####
+
+# one stock
+LDp1 = {
+    "TSLA" : 5000,
+}
+
+# heavy on one stock
+LDp2 = {
+    "TSLA" : 5000,
+    "APPL" : 10,
+    "COO" : 10,
+}
+
+# automobile industry
+LDp3 = {
+    "TSLA" : 5000,
+    "NIO" : 5000,
+    "GM" : 5000,
+}
+
+# tech industry
+LDp4 = {
+    "AMZN" : 5000,
+    "FB" : 5000,
+    "GOOGL" : 5000,
+    "NFLX" : 5000,
+}
+
+# bank industry
+LDp5 = {
+    "BAC" : 5000,
+    "JPM" : 5000,
+    "WFC" : 5000,
+    "C" : 5000,
+    "GS" : 5000,
+}
+
